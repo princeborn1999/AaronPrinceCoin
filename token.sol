46182b9063daa272af0c4177d6a454389aa2c968
AaronPrince <princeborn1999@gmail.com>
2022-01-22 08:37:39 +0800

    token.sol
    code

---
+++ token.sol
@@ -0,0 +1,7 @@
+echo "# AaronPrinceCoin" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/princeborn1999/AaronPrinceCoin.git
+git push -u origin main
